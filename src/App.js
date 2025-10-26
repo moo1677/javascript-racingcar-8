16f02cb4529de759ebca2498fabc41dd7206fb15
moo1677 <lgm04@naver.com>
2025-10-26 14:38:33 +0900

    feat : 문자열 입력 및 분할

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Console, MissionUtils } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    let car_str = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+    );
+    car_str = car_str.split(",");
+  }
 }
 
 export default App;
