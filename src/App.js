c9e68935d216f87f7209fbcb5335a76ed04f0903
moo1677 <lgm04@naver.com>
2025-10-26 17:40:29 +0900

    refactor : 함수 단위로 모듈화

--- src/App.js
+++ src/App.js
@@ -2,31 +2,51 @@ import { Console, MissionUtils } from "@woowacourse/mission-utils";
 
 class App {
   async run() {
-    const car_array = [];
-    let car_str = await Console.readLineAsync(
-      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
-    );
-    const num_race = await Console.readLineAsync(
-      "시도할 횟수는 몇 회인가요?\n"
-    );
-    car_str = car_str.split(",");
-    car_str.forEach((element) => {
-      car_array.push({ car_name: element, step: 0 });
-    });
-    runRacingGame(car_array, num_race);
+    try {
+      let car_str = await inputCarStr();
+      const num_race = await inputNumRace();
+      const car_array = [];
+      initRacingGame(car_str, car_array);
+      runRacingGame(car_array, num_race);
+    } catch {}
   }
 }
+const initRacingGame = (car_str, car_array) => {
+  car_str = splitCarArray(car_str);
+  createCarObj(car_str, car_array);
+};
 
+const inputCarStr = () => {
+  return Console.readLineAsync(
+    "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"
+  );
+};
+const inputNumRace = () => {
+  return Console.readLineAsync("시도할 횟수는 몇 회인가요?\n");
+};
+
+const splitCarArray = (car_str) => {
+  car_str = car_str.split(",");
+  return car_str;
+};
+const createCarObj = (car_str, car_array) => {
+  car_str.forEach((element) => {
+    car_array.push({ car_name: element, step: 0 });
+  });
+};
 const runRacingGame = (car_array, num_race) => {
   let step_max = 1;
   const winner = [];
   Console.print("\n실행 결과");
+  startRace(car_array, num_race, step_max);
+  winner.push(...decideWinner(car_array, step_max));
+  winnerPrint(winner);
+};
+const startRace = (car_array, num_race, step_max) => {
   for (let i = 0; i < num_race; i++) {
     step_max = moveCar(car_array, step_max);
     printCarMove(car_array);
   }
-  winner.push(...decideWinner(car_array, step_max));
-  winnerPrint(winner);
 };
 const moveCar = (car_array, step_max) => {
   car_array.forEach((e) => {
@@ -55,7 +75,6 @@ const printCarMove = (car_array) => {
 const decideWinner = (car_array, step_max) => {
   const winner_array = [];
   car_array.map((e) => winner_array.push(selectWinner(e, step_max)));
-  Console.print(winner_array);
   return winner_array.filter(Boolean);
 };
 const selectWinner = (car, step_max) => {
@@ -63,6 +82,5 @@ const selectWinner = (car, step_max) => {
 };
 const winnerPrint = (winner) => {
   Console.print(`최종 우승자 : ${winner.join(", ")}`);
-  Console.print(winner);
 };
 export default App;
