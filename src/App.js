8b7d18f1e444d78e267d037e86af86d5b9f752c8
moo1677 <lgm04@naver.com>
2025-10-26 17:00:25 +0900

    feat : 우승한 자동차 이름 출력

--- src/App.js
+++ src/App.js
@@ -13,25 +13,56 @@ class App {
     car_str.forEach((element) => {
       car_array.push({ car_name: element, step: 0 });
     });
-    runRacingGame(num_race, car_array);
+    runRacingGame(car_array, num_race);
   }
 }
-const runRacingGame = (num_race, car_array) => {
+
+const runRacingGame = (car_array, num_race) => {
+  let step_max = 1;
+  const winner = [];
   Console.print("\n실행 결과");
   for (let i = 0; i < num_race; i++) {
-    moveCar(car_array);
+    step_max = moveCar(car_array, step_max);
+    printCarMove(car_array);
   }
+  winner.push(...decideWinner(car_array, step_max));
+  winnerPrint(winner);
 };
-const moveCar = (car_array) => {
+const moveCar = (car_array, step_max) => {
   car_array.forEach((e) => {
     if (decideRandomly()) {
       e.step++;
     }
+    step_max = decideMaxStep(step_max, e.step);
   });
+  return step_max;
 };
 const decideRandomly = () => {
   const random_num = MissionUtils.Random.pickNumberInRange(0, 9);
   return random_num >= 4;
 };
+const decideMaxStep = (max, step) => {
+  if (max < step) return step;
+  return max;
+};
+const printCarMove = (car_array) => {
+  car_array.forEach((e) => {
+    Console.print(`${e.car_name} : ${"-".repeat(e.step)}`);
+  });
+  Console.print("\n");
+};
 
+const decideWinner = (car_array, step_max) => {
+  const winner_array = [];
+  car_array.map((e) => winner_array.push(selectWinner(e, step_max)));
+  Console.print(winner_array);
+  return winner_array.filter(Boolean);
+};
+const selectWinner = (car, step_max) => {
+  if (car.step === step_max) return car.car_name;
+};
+const winnerPrint = (winner) => {
+  Console.print(`최종 우승자 : ${winner.join(", ")}`);
+  Console.print(winner);
+};
 export default App;
