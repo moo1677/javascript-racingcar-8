1fdde275f51633d9b9086f44cc1a2436dfcb0312
moo1677 <lgm04@naver.com>
2025-10-27 12:58:28 +0900

    feat : 입력된 자동차의 대수가 1대 이하일 때 예외 처리

--- src/checkInput.js
+++ src/checkInput.js
@@ -1,4 +1,6 @@
 export const checkCarNames = (names) => {
+  if (names.length <= 1)
+    throw new Error("경주를 하려면 2대 이상의 자동차가 필요합니다");
   names.forEach((name) => {
     if (!name) throw new Error("유효한 자동차의 이름을 입력해주세요");
     if (name.length > 6 || name.length < 1)
